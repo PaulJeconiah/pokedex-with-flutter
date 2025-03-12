8fa0f3761ec56c7919a4f7ffabbdc83d4c5fc8e7
Paul Jeconiah <pauljack1672@gmail.com>
2025-03-12 20:50:22 +0700

    Mencoba membuat home page

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFDC0A2D),
+      body: Column(children: [AppBar()]),
+    );
+  }
+}
