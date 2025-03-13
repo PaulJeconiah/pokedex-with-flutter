606d4e13f48d80525147e328d861e024bb8d913e
Paul Jeconiah <pauljack1672@gmail.com>
2025-03-13 21:45:54 +0700

    Menambahkan beberapa fitur di homePage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -7,7 +7,46 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFFDC0A2D),
-      body: Column(children: [AppBar()]),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 12, top: 20, right: 12),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Image.asset(
+                    "assets/images/Pokeball.png",
+                    width: 24,
+                    height: 24,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 16),
+                    child: Text(
+                      "Pokédex",
+                      style: TextStyle(
+                        fontFamily: "Poppins",
+                        fontSize: 24,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 25),
+              Expanded(
+                child: ClipRRect(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(15),
+                    topRight: Radius.circular(15),
+                  ),
+                  child: Container(color: Colors.white),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
