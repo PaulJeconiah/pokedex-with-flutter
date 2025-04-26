39c9e05d2abf9f41c77c1bdda85801fb27f5e817
Paul Jeconiah <pauljack1672@gmail.com>
2025-04-26 17:43:39 +0700

    Menambahkan assets dan components

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:pokedex_with_flutter/components/theme.dart';
 
 import 'app/routes/app_pages.dart';
 
 void main() {
   runApp(
     GetMaterialApp(
+      theme: pokemonTheme,
       title: "Application",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
